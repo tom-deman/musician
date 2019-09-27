77f2f0e2aaacafa2b544b790d7d90d93748eec90
Tom-deman <dt.deman.thomas@gmail.com>
2019-09-27 14:25:44 +0200

    Updates

---
+++ precache-manifest.90dcb0f694d4ad9302064a8311feca74.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789bc4c439e1ed812557b69a3af97f9b",
+    "url": "/musician/index.html"
+  },
+  {
+    "revision": "da01817008a048382e32",
+    "url": "/musician/static/css/2.c51298e9.chunk.css"
+  },
+  {
+    "revision": "b9ea8e1265bcb42cea46",
+    "url": "/musician/static/css/main.4100bed0.chunk.css"
+  },
+  {
+    "revision": "da01817008a048382e32",
+    "url": "/musician/static/js/2.e02322ab.chunk.js"
+  },
+  {
+    "revision": "b9ea8e1265bcb42cea46",
+    "url": "/musician/static/js/main.f93b1339.chunk.js"
+  },
+  {
+    "revision": "a89f1893c87a8c6672a5",
+    "url": "/musician/static/js/runtime-main.e076c4f6.js"
+  },
+  {
+    "revision": "a11c2910c4092f88252078d313ca9800",
+    "url": "/musician/static/media/1.a11c2910.jpg"
+  },
+  {
+    "revision": "2d72a94d89c775327a213797e781511a",
+    "url": "/musician/static/media/10.2d72a94d.jpg"
+  },
+  {
+    "revision": "f780080d16addb4987167b0b9e5a7191",
+    "url": "/musician/static/media/2.f780080d.jpg"
+  },
+  {
+    "revision": "930129103e20cc1715052f52832eb067",
+    "url": "/musician/static/media/3.93012910.jpg"
+  },
+  {
+    "revision": "aebed2af2be29cb0ec618a4010c8d0d5",
+    "url": "/musician/static/media/4.aebed2af.jpg"
+  },
+  {
+    "revision": "c23adf268ec126149cb21845b8b37f23",
+    "url": "/musician/static/media/5.c23adf26.jpg"
+  },
+  {
+    "revision": "a3d39250d853cee6a81a1b6dea477794",
+    "url": "/musician/static/media/6.a3d39250.jpg"
+  },
+  {
+    "revision": "50a9a4522a30ea9f1e9d8a22fb7290eb",
+    "url": "/musician/static/media/7.50a9a452.jpg"
+  },
+  {
+    "revision": "73fcb5a3730c74ab0c090aa5a5fa20b6",
+    "url": "/musician/static/media/8.73fcb5a3.jpg"
+  },
+  {
+    "revision": "44e8852bc1278eb2aefcd5017b436bd2",
+    "url": "/musician/static/media/9.44e8852b.jpg"
+  },
+  {
+    "revision": "474ad4a9422d7ce7dfa830c23d22ec09",
+    "url": "/musician/static/media/banner.474ad4a9.jpg"
+  },
+  {
+    "revision": "0425d661f34ffa46604c9dfa344c03bb",
+    "url": "/musician/static/media/fa-brands-400.0425d661.woff2"
+  },
+  {
+    "revision": "189c0326f75931886d5b60cde8b5628f",
+    "url": "/musician/static/media/fa-brands-400.189c0326.ttf"
+  },
+  {
+    "revision": "3a19eb0134e93ea0caa5826f5fb7b642",
+    "url": "/musician/static/media/fa-brands-400.3a19eb01.svg"
+  },
+  {
+    "revision": "451b51d8700fb035bcaaa9e8710b384a",
+    "url": "/musician/static/media/fa-brands-400.451b51d8.eot"
+  },
+  {
+    "revision": "85b063debeb5d5f079776ac1a9c924cb",
+    "url": "/musician/static/media/fa-brands-400.85b063de.woff"
+  },
+  {
+    "revision": "14f22fbed8146dbf3b497bb32d8491b0",
+    "url": "/musician/static/media/fa-regular-400.14f22fbe.woff"
+  },
+  {
+    "revision": "772a0f14c850c8b0dfe283a95857583e",
+    "url": "/musician/static/media/fa-regular-400.772a0f14.woff2"
+  },
+  {
+    "revision": "8cd2768111e3091948920a3ad11f96c6",
+    "url": "/musician/static/media/fa-regular-400.8cd27681.svg"
+  },
+  {
+    "revision": "9230982faf6ed4d1dab07babfb813777",
+    "url": "/musician/static/media/fa-regular-400.9230982f.ttf"
+  },
+  {
+    "revision": "b6579220162705be426ad4db0dc43f6b",
+    "url": "/musician/static/media/fa-regular-400.b6579220.eot"
+  },
+  {
+    "revision": "1898ebd2e3162e7ca8627073d865b893",
+    "url": "/musician/static/media/fa-solid-900.1898ebd2.eot"
+  },
+  {
+    "revision": "1d825f28ae4e412886fb0123b94a8ac2",
+    "url": "/musician/static/media/fa-solid-900.1d825f28.svg"
+  },
+  {
+    "revision": "4cc04a31c42f2f9d951547bbce75960b",
+    "url": "/musician/static/media/fa-solid-900.4cc04a31.woff2"
+  },
+  {
+    "revision": "b1161bce5381a5fa7aacc073e1a01dd3",
+    "url": "/musician/static/media/fa-solid-900.b1161bce.ttf"
+  },
+  {
+    "revision": "db525089f5101b59a66184d109e6f917",
+    "url": "/musician/static/media/fa-solid-900.db525089.woff"
+  },
+  {
+    "revision": "460c7830e8c82355c803c2475a4db0a3",
+    "url": "/musician/static/media/footer.460c7830.jpg"
+  },
+  {
+    "revision": "936aa7620ed34322534e503f6537a95b",
+    "url": "/musician/static/media/header.936aa762.jpg"
+  },
+  {
+    "revision": "a3b24254f83e8bff94493902d5a75058",
+    "url": "/musician/static/media/one.a3b24254.jpg"
+  },
+  {
+    "revision": "28e6ad1357c8380cfb42e2b9fbd8d9cc",
+    "url": "/musician/static/media/trashhand.28e6ad13.TTF"
+  },
+  {
+    "revision": "9f0541cfe4695867791fbde9ad289bf2",
+    "url": "/musician/static/media/two.9f0541cf.jpg"
+  }
+]);
