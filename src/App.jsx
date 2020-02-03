af370b35e9f00ba232415daea01838542a6dc7e9
Thomas <dt.deman.thomas@gmail.com>
2020-02-03 15:48:54 +0100

    push before refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
 
 
 export default class App extends Component {
-     
+
      render() {
           return (
                <div>
